babafa02aef3aa60d4ddcff078796c5885d371de
nkaurelien <nkaurelien@gmail.com>
2024-11-23 17:32:57 +0100

    Ajout de Clés publiques SSH

--- aim_user/main.tf
+++ aim_user/main.tf
@@ -16,3 +16,15 @@ resource "aws_iam_user" "admin_user" {
   path = "/internal/"
 }
 
+# resource "aws_key_pair" "nkaurelien" {
+#   key_name   = "user${aws_iam_user.nkaurelien.path}${aws_iam_user.nkaurelien.name}"
+#   public_key = file("${path.module}/ssh-key/nkaurelien.pub")
+# }
+
+
+resource "aws_iam_user_ssh_key" "nkaurelien" {
+  username   = aws_iam_user.nkaurelien.name
+  encoding   = "SSH"
+  public_key = file("${path.module}/ssh-key/nkaurelien.pub")
+  # public_key = aws_key_pair.nkaurelien.public_key
+}
